184cf03d251b176d866a9e357e06a677ecfc1267
Juan Estay <juan.estay.rodriguez@icloud.com>
2026-04-04 00:53:43 -0300

    update railway

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
-import "./src/config/env.js"
+import dns from 'node:dns';
+// Esta línea es la clave: obliga a Node.js a preferir IPv4 sobre IPv6
+dns.setDefaultResultOrder('ipv4first');
 
+import "./src/config/env.js"
 import app from "./src/app.js"
 import pool from "./src/database/db.js"
 
@@ -7,10 +10,13 @@ const PORT = process.env.PORT || 4000
 
 const startServer = async () => {
   try {
+    // Verificamos conexión
     await pool.query("SELECT 1")
+    console.log("✅ Conexión a base de datos exitosa")
 
-    app.listen(PORT, () => {
-      console.log(`🚀 Servidor corriendo en http://localhost:${PORT}`)
+    // Escuchamos en '0.0.0.0' para asegurar que Railway pueda rutear el tráfico
+    app.listen(PORT, '0.0.0.0', () => {
+      console.log(`🚀 Servidor corriendo en puerto ${PORT}`)
     })
 
   } catch (error) {
